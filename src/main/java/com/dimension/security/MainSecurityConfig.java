0ac4d09963bd3ced1b211be8431a3adf0e69b952
michaelhidalgom <mhidalgo.a6@gmail.com>
2025-05-27 16:41:40 -0500

    refactor: modified security

--- src/main/java/com/dimension/security/MainSecurityConfig.java
+++ src/main/java/com/dimension/security/MainSecurityConfig.java
@@ -64,46 +64,26 @@ public class MainSecurityConfig extends WebSecurityConfigurerAdapter {
    */
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
     http
-            //El filtro csrf esta habilitado por defecto, asi que se deshabilita
-            //porque se va a utilizar otro mecanismo de token
-            //.csrf().disable()
-
             .csrf()
-            .ignoringAntMatchers("/h2-console/**", "/api/**") // Ignora CSRF para H2 Console y autenticación
+            .ignoringAntMatchers("/h2-console/**", "/api/**")
             .and()
 
-            //Permite configurar el manejo de excepciones
             .exceptionHandling()
-            //El manejo de excepciones se delega a un AuthenticationEntryPoint
             .authenticationEntryPoint(jwtAuthenticationEntryPoint)
             .and()
 
-            //Permite configurar la gestion de sesiones
             .sessionManagement()
-            //Se le especifica a Spring que deshabilite el uso de sesiones
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
 
-            //Le dice a spring que reglas usar al autorizar solicitudes,
             .authorizeRequests()
-                //Los recursos en cuya URL se le antecede "public" no requieren autenticacion.
-                .antMatchers("/api/public/**",
-                        "/api/encryption/**",
-                        "/h2-console/**",
-                        "/api/auth/**").permitAll()
-            //Todas las demas URLs de la aplicacion requieren autenticacion
+            .antMatchers("/api/auth/**", "/h2-console/**").permitAll()
             .anyRequest().authenticated()
             .and()
-        .headers()
-            .frameOptions().sameOrigin() // Permite el uso de frames para H2 Console
-            .and()
-        .headers()
-            .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+            .headers()
+            .frameOptions().sameOrigin();
 
-    //Permite agregar un filtro (jwtAuthenticationFilter) personalizado antes del
-    //filtro UsernamePasswordAuthenticationFilter
     http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
   }
 }
